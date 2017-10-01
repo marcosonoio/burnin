bd4756305d82992ff320204fa6135d2db732a7b2
marcosonoio <marcogiacinti1@gmail.com>
2017-10-02 01:02:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,18 +12,18 @@ install -m 755 etc/udev/rules.d/* /etc/udev/rules.d/
 install -m 755 etc/systemd/system/* /etc/systemd/system/
 install -m 755 usr/local/bin/* /usr/local/bin/
 
-#read -r -p 'set directory path to save report ( /home/${SUDO_USER}/burnin ): ' config
-#if [ [ -n "${config}" ]; then
-#   echo "report_path=${config}" > /etc/burnin
-#else
-#   echo "report_path=/home/${SUDO_USER}/burnin"
-#fi
+read -r -p 'set directory path to save report ( /home/${SUDO_USER}/burnin ): ' config
+if [ -n "${config}" ] ; then
+   echo "report_path=${config}" > /etc/burnin
+else
+   echo "report_path=/home/${SUDO_USER}/burnin" > /etc/burnin
+fi
 #read -r -p 'set the physical port that you want to monitor (ex.: "ata1 ata2 ata3 ata4"), 
 
-if [ ! -f /etc/burnin ]; then
-  read -r -p 'set directory path to save report:\n' config
-  echo "${config}" > /etc/burnin
-fi
+#if [ ! -f /etc/burnin ]; then
+#  read -r -p 'set directory path to save report:\n' config
+#  echo "${config}" > /etc/burnin
+#fi
 
 config=$(cat /etc/burnin)
 echo "the path to save the report is:"
